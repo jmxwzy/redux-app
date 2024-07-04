59efee44f45912c3b563f7767965b00ff62d0bd6
jmxwzy <1140312750@qq.com>
2024-07-04 20:09:01 +0800

    f3组合f1和f2

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from '@reduxjs/toolkit';
 
 // state：维护的数据，一般维护成树的结构。
 // action：一个普通对象，存储reducer的传入参数，一般描述对state的更新类型。
@@ -27,19 +28,33 @@ const f2 = (state = "", action) => {
   }
 };
 
+// f3用来组合f1和f2
+// const f3 = (state = {}, action) => {
+//   return {
+//     f1: f1(state.f1, action),
+//     f2: f2(state.f2, action),
+//   }
+// };
+const f3 = combineReducers({
+  f1: f1,
+  f2: f2,
+});
+
 const store = configureStore({
-  reducer: f1, // reducer：对state进行更新的函数，每个state绑定一个reducer。传入两个参数：当前state和action，返回新state。
+  reducer: f3, // reducer：对state进行更新的函数，每个state绑定一个reducer。传入两个参数：当前state和action，返回新state。
 });
 
 console.log("Start: ", store.getState());
 
 store.subscribe(() => {console.log("After dispatch: ", store.getState())}); // subscribe: 每次dispatch后执行
 
-store.dispatch({type: 'add', value: 1}); // dispatch：传入一个参数action，对整棵state树操作一遍。
+store.dispatch({type: 'add', value: 1}); // dispatch：传入一个参数action，对整棵state树操作一遍(递归调用)。
 store.dispatch({type: 'add', value: 2});
 store.dispatch({type: 'add', value: 3});
 store.dispatch({type: 'sub', value: 4});
 store.dispatch({type: 'sub', value: 5});
+store.dispatch({type: 'concat', character: 'wzy'});
+store.dispatch({type: 'concat', character: 'wzy'});
 
 console.log("final: ", store.getState());
 
